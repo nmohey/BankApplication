f20c640cfec1d0a1f444c6ac72072976e8edef14
Nada <nada.abdelal@student.guc.edu.eg>
2025-08-10 21:16:39 +0300

    organized the code in one place

--- Users/ahmed/Downloads/BankApplication-main/BankApplication-main/src/main/java/com/sprints/BankApplication/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sprints.BankApplication.repository;
-
-import com.sprints.BankApplication.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
